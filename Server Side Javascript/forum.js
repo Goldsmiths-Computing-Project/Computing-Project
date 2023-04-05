42a3653986ffcd740184c00ff8fa2d2a7094e571
gracemaxx <124292563+gracemaxx@users.noreply.github.com>
2023-04-05 18:09:51 +0100

    Update forum.js

--- Server Side Javascript/forum.js
+++ Server Side Javascript/forum.js
@@ -1,22 +1,29 @@
 // Route handler for web app
 module.exports = function(app) {
 
- //The Code for forum page goes here
- //Render page
- app.get('/forum',function(req,res){
-  let sqlquery = `SELECT   p.post_id, p.post_date, t.topic_title p.post_content, u.username
-                  FROM     posts p
-                  LEFT JOIN topics t
-                  ON       p.topic_id = t.topic_id
-                  LEFT JOIN users u
-                  ON       p.user_id = u.user_id`;
-  db.query(sqlquery, (err, result) => {
+//The Code for forum page goes here
+
+///Requiring Modules
+const session = require('express-session');
+
+
+///Creating Session
+app.use(session({
+  secret: 'mysecretkey',
+  resave: false,
+  saveUninitialized: true
+}));
+
+//Render forum page
+app.get('/forum', (req, res) => {
+  const sql = 'SELECT * FROM forum';
+  db.query(sql, (err, results) => {
     if (err) {
-       res.redirect('./');
+      throw err;
     }
-    let data = Object.assign({}, {posts:result});
-    console.log(data)
-    res.render('forum.ejs', data);
+    const searchResults = { forum: [], forum: results };
+    res.render('forum', searchResults);
   });
 });
 }
+ 
