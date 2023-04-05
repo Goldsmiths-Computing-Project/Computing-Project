6bb6764d398481a8f092b8d0da30944bcb9f10bc
gracemaxx <124292563+gracemaxx@users.noreply.github.com>
2023-04-05 15:48:05 +0100

    Update forum.js

--- Server Side Javascript/forum.js
+++ Server Side Javascript/forum.js
@@ -2,11 +2,21 @@
 module.exports = function(app) {
 
  //The Code for forum page goes here
- 
-    //Render page
-   app.get('/forum', function(req, res) {
-        res.render('forum.ejs');
-      });
-  
-
+ //Render page
+ app.get('/forum',function(req,res){
+  let sqlquery = `SELECT   p.post_id, p.post_date, t.topic_title p.post_content, u.username
+                  FROM     posts p
+                  LEFT JOIN topics t
+                  ON       p.topic_id = t.topic_id
+                  LEFT JOIN users u
+                  ON       p.user_id = u.user_id`;
+  db.query(sqlquery, (err, result) => {
+    if (err) {
+       res.redirect('./');
+    }
+    let data = Object.assign({}, {posts:result});
+    console.log(data)
+    res.render('forum.ejs', data);
+  });
+});
 }
